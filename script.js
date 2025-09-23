3fadf9b6a540ae72c12148d95603deb515765a94
eduarda121212 <eduarda.n.silva11@aluno.senai.br>
2025-09-23 16:24:44 -0300

    rolagem no js

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ card.addEventListener('mouseleave', function() {
     this.style.transform = 'translateY(0)';
 });
 });
+
+//Adicione rolagem suave para links de navegação
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
